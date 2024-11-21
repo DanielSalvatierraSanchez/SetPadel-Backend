179fa35e391011f3f47ad04dcb460fff31436752
dsalvatierra <dss250583@gmail.com>
2024-11-21 23:06:43 +0100

    FEAT File getPagination

---
+++ src/utils/getPagination.js
@@ -0,0 +1,14 @@
+const PadelMatch = require("../api/models/padelMatches");
+
+const getPagination = async (query) => {
+    const totalPages = await PadelMatch.countDocuments(query);
+    const lastPage = Math.floor(total / 6) + 1;
+    return {
+        totalPages,
+        lastPage
+    };
+};
+
+module.exports = {
+    getPagination
+};
