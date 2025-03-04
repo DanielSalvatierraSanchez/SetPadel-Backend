d6a76a4495d3fcc4ca1b649dc690c708ea3e6c88
Daniel Salvatierra <dss250583@gmail.com>
2025-03-04 18:10:23 +0100

    replace dotenv for process.loadEvnFile()

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL)
-        console.log('✅ BBDD Connected');
+        await mongoose.connect(process.env.DB_URL);
+        console.log("✅ BBDD Connected");
     } catch (error) {
-        console.log('❌ BBDD Error');
+        console.log("❌ BBDD Error");
     }
-}
+};
 
-module.exports = { connectDB }
+module.exports = { connectDB };
